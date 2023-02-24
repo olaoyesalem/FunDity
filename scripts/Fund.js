bc7509b056595f27a048c282a9cc45ca1c9843e6
Olaoye Salem <89555234+olaoyesalem@users.noreply.github.com>
2023-02-25 00:01:03 +0100

    Update Fund.js

--- scripts/Fund.js
+++ scripts/Fund.js
@@ -14,7 +14,7 @@ async function main(){
     console.log("Stil Deploying.......")
     const Fund = await FundMe.Fund({value: sendValue});
     Fund.wait(1);
-    console.log(" Funde d ");
+    console.log(" Funded ");
     
 
 }

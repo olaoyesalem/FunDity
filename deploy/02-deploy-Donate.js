87944ce4588686237d0e1cb1bfe67e2e0aef57de
Olaoyesalem <olaoyesalemgreat@gmail.com>
2023-03-04 03:16:56 +0100

    updated Deploy-DonateFactory.js

--- deploy/02-deploy-Donate.js
+++ deploy/02-deploy-Donate.js
@@ -5,4 +5,17 @@ module.exports = async ({getNamedAccounts,deployments})=>{
 
     const{log,deploy} = deployments
     const{deployer} = getNamedAccounts()
-}
+
+    const DonateFactory = await deploy("DonateFactory",{
+        from:deployer,
+        log:true,
+        args:[]
+
+    })
+
+    if(!developmentChains.includes(network.name)&&process.env.ETHERSCAN_API_KEY){
+        await verify(DonateFactory.address,args )
+    }
+    log( `Deployer : ${deployer}`);
+    log("--------------------------");
+};

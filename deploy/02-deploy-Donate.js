510ea8f7ebcfa6f87177ffa34908215f292250da
Olaoyesalem <olaoyesalemgreat@gmail.com>
2023-03-04 03:11:48 +0100

    Updated Deploy-Donate.js

--- deploy/02-deploy-Donate.js
+++ deploy/02-deploy-Donate.js
@@ -1,4 +1,8 @@
 const {network}= require("hardhat")
 const {verify}= require("../utils/verify")
 
-module.exports = async ({getNamedAccounts,deployments})=>{}
+module.exports = async ({getNamedAccounts,deployments})=>{
+
+    const{log,deploy} = deployments
+    const{deployer} = getNamedAccounts()
+}

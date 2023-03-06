874e037902f8c38f8ec2c788f1564a0eb6aa40b6
Olaoyesalem <olaoyesalemgreat@gmail.com>
2023-03-06 09:53:48 +0100

    Updated Donate.test

--- test/Fund.staging.test.js
+++ test/Fund.staging.test.js
@@ -1,7 +1,12 @@
 const { assert } = require('chai')
-const {network, ethers} = require('hardhat')
-const {developmentChains, networkConfig,campaignName,description,recipient} = require('../helper-hardhat.config')
-
+const { network, ethers } = require('hardhat')
+const {
+    developmentChains,
+    networkConfig,
+    campaignName,
+    description,
+    recipient,
+} = require('../helper-hardhat.config')
 
 !developmentChains.includes(network.name)
     ? describe.skip
@@ -14,52 +19,51 @@ const {developmentChains, networkConfig,campaignName,description,recipient} = re
               )
               const donateContract = await ethers.getContractFactory('Donate')
               console.log('Deploying ..........')
-              Donate = await donateContractFactory.deploy()
-              await Donate.deployed
+              donateFactory = await donateContractFactory.deploy()
+              donateContract = await donateContract.deploy(campaignName,description,recipient);
+              await donateFactory.wait(1)
+              await donateContract.wait(1)
+              await donateFactory.deployed
+              await donateContract.deployed.
               console.log(`Deployed To ${Donate.address} `)
           })
           it.only(' should allow people to fund', async function () {
               await Donate.Fund({ value: sendValue })
               console.log('Funded!!!')
           })
-        })
-          it('should only the owner to withdraw', async function () {
+      })
+it('should only the owner to withdraw', async function () {
+    await Donate.Fund({ value: sendValue })
+    await Donate.withdraw()
+    endingFundMeBalance = await ethers.provider.getBalance(Donate.address)
+    assert.equal(endingFundMeBalance.toString(), '0')
+    console.log('Witdrawn!!!!!!!')
+})
 
-          
-              await Donate.Fund({ value: sendValue })
-              await Donate.withdraw()
-              endingFundMeBalance = await ethers.provider.getBalance(
-                  Donate.address
-              )
-              assert.equal(endingFundMeBalance.toString(), '0')
-              console.log('Witdrawn!!!!!!!')
-          })
+//       it('should check if the address created is correctly mapped', async function () {
+//           addressName = 'Salem'
+//           await Donate.createMyFunDity(addressName)
+//           const addressCreated = await Donate.listOfFunDityAddresses(0)
+//           const txnResponse = await Donate.nameToAddress(addressName)
+//           assert.equal(addressCreated, txnResponse)
+//           console.log(' Address Successfully created !!!')
+//       })
+//       it('should fund the address created',async function(){
+//         addressName = 'Salem'
+//           await Donate.createMyFunDity(addressName)
+//           const address = await Donate.listOfFunDityAddresses(0)
+//          await Donate.fundAddress(address)
+//          console.log('Successfully Funded!!!')
 
-    //       it('should check if the address created is correctly mapped', async function () {
-    //           addressName = 'Salem'
-    //           await Donate.createMyFunDity(addressName)
-    //           const addressCreated = await Donate.listOfFunDityAddresses(0)
-    //           const txnResponse = await Donate.nameToAddress(addressName)
-    //           assert.equal(addressCreated, txnResponse)
-    //           console.log(' Address Successfully created !!!')
-    //       })
-    //       it('should fund the address created',async function(){
-    //         addressName = 'Salem'
-    //           await Donate.createMyFunDity(addressName)
-    //           const address = await Donate.listOfFunDityAddresses(0)
-    //          await Donate.fundAddress(address)
-    //          console.log('Successfully Funded!!!')
+//       })
+//       it.only('should check the balance of the address',async function(){
+//         addressName = 'Salem'
+//         await Donate.createMyFunDity(addressName)
+//         const address = await Donate.listOfFunDityAddresses(0)
+//         const txnResponse = await Donate.getAddressBalance(address)
 
-              
-    //       })
-    //       it.only('should check the balance of the address',async function(){
-    //         addressName = 'Salem'
-    //         await Donate.createMyFunDity(addressName)
-    //         const address = await Donate.listOfFunDityAddresses(0)
-    //         const txnResponse = await Donate.getAddressBalance(address)
+//         assert.equal(txnResponse.toString(),"0")
+//       })
+//   })
 
-    //         assert.equal(txnResponse.toString(),"0")
-    //       })
-    //   })
-
-      // remains just the withdrawAddress functitton
+// remains just the withdrawAddress functitton
